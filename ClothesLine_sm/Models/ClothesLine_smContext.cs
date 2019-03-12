a7c35de492a816cfc5f514de92abf318c914db66
caesar-c4 <caesar13690@gmail.com>
2019-03-12 00:32:15 -0700

    Initial commit

---
+++ ClothesLine_sm/Models/ClothesLine_smContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ClothesLine_sm.Models
+{
+    public class ClothesLine_smContext:DbContext
+    {
+        public ClothesLine_smContext() : base("ClothesLine_sm")
+        {
+
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<CustomerOrder> CustomerOrders { get; set; }
+
+        public DbSet<OrderedProduct> Orderedproducts { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    }
+}
---
+++ ClothesLine_sm/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClothesLine_sm.Models;
+
+namespace ClothesLine_sm.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
